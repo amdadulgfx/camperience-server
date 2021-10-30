f847f92040a286023b08dcb60798268618d53943
Md Amdadul Haq Arif <79464808+amdadulgfx@users.noreply.github.com>
2021-10-30 17:25:03 +0600

    single data read

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific id', id);
+            const query = { _id: ObjectId(id) }
+            const service = await campCollection.findOne(query);
+            res.json(service);
+        })
     }
     finally {
         //await client.close();
